d2fda2e57e286a383fda80acf0b703c411a41b95
Tim Thayne <timthayne@gmail.com>
2024-04-25 15:47:09 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+var firstName = 'Brother Thayne';
+function sayHello(name) {
+    console.log("Hello, ".concat(name, "!"));
+}
+sayHello(firstName);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const firstName: string = 'Brother Thayne';
+
+function sayHello(name: string) {
+  console.log(`Hello, ${name}!`);
+}
+
+sayHello(firstName);
